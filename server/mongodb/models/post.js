c5eb7888604071da85d87a1609a619a0e99b298b
codethat19 <aayushrastogi1997@gmail.com>
2023-03-06 21:48:10 +0530

    add login branch, setup mongoose Schema, update DB connect URL

--- server/mongodb/models/post.js
+++ server/mongodb/models/post.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 const Post = new mongoose.Schema({
     name: { type: String, required: true },
     prompt: { type: String, required: true },
-    photo: { type: String, required: true }
+    photo: { type: String, required: true },
+    user: {type: String, required: true},
+    flag: { type: Number, required: true, default: 1}
 });
 
 const PostSchema = mongoose.model('Post', Post);
